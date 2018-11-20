28b2f04e43e2850ea3b77ac86c3059b320f4169c
cin4858 <cin4858@naver.com>
2018-11-20 13:48:49 +0900

    ... changed...

--- blog/forms.py
+++ blog/forms.py
@@ -8,5 +8,9 @@ class PostSearchForm(forms.Form):  # forms 덕분에 검색어에 네모난 폼�
     # CharField는 TextInput 위젯으로 표현됨
     # TextInput 위젯 앞에 label이 표시되고,
     # 변수 search_word는 입력 필드에 대한 id 역할
-    search_word = forms.CharField(label='검색어')  # 폼 앞에 "검색어" 라고 붙여지는 것, 이것만 붙이면 콜론과 "확인"이 자동으로 붙여짐
+    search_word = forms.CharField(label='제목 검색어')  # 폼 앞에 "검색어" 라고 붙여지는 것, 이것만 붙이면 콜론과 "확인"이 자동으로 붙여짐
+    search_word2 = forms.CharField(label='요약 검색어')
+    search_word3 = forms.CharField(label='본문 검색어')
+    search_word4 = forms.CharField(label='태그 검색어')
+
 # ch09 1/1 추가 종료
